df12e0810a43c01ae5cf298af39eb36df7e334aa
madanreddy816 <madanreddy22300@gmail.com>
2024-03-13 21:37:30 +0530

    day7_learning

---
+++ src/com/learning/core/day7/D07P04.java
@@ -0,0 +1,60 @@
+package com.learning.day7;
+import java.util.Scanner;
+import java.util.Stack;
+public class D07P04 {
+
+  public static void main(String[] args) 
+  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the expression:");
+        String expression = scanner.nextLine();
+        int result = evaluateExpression(expression);
+        System.out.println("Output:");
+        System.out.println(result);
+    }
+    public static int evaluateExpression(String expression)
+    {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0;
+        char sign = '+';
+
+        for (int i = 0; i < expression.length(); i++) 
+        {
+            char ch = expression.charAt(i);
+
+            if (Character.isDigit(ch)) 
+            {
+                num = num * 10 + (ch - '0');
+            }
+
+            if ((!Character.isDigit(ch) && ch != ' ') || i == expression.length() - 1) {
+                if (sign == '+') 
+                {
+                    stack.push(num);
+                }
+                else if (sign == '-') 
+                {
+                    stack.push(-num);
+                }
+                else if (sign == '*') 
+                
+                {
+                    stack.push(stack.pop() * num);
+                } 
+                else if (sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+
+                sign = ch;
+                num = 0;
+            }
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) 
+        {
+            result += stack.pop();
+        }
+        return result;
+    }
+}
